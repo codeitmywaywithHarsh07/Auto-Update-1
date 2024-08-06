32f8e30ad5f6e41e3576e45931d8e91ca0492b57
Harsh-winuall <harsh@winuall.com>
2024-08-06 17:33:32 +0530

    Releasing v1.0.1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
     <div>
         <h1>Welcome to Harsh's App</h1>
         <p>I hope you are doing Well!</p>
+        <hr />
+        <p>This is the new Updated Version</p>
     </div>
   )
 }
