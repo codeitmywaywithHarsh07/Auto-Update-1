be3b02316db4fc147f5c0c6b3e126fe75a7a3234
Harsh-winuall <harsh@winuall.com>
2024-08-06 16:26:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Welcome to Harsh's App</h1>
+        <p>I hope you are doing Well!</p>
+    </div>
+  )
+}
+
+export default App
